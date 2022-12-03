6f5136411f6f3c534debac520bd7234fb7f1eb61
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2022-12-03 21:45:37 +0600

    connection mongodb

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
+
+// connection mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cbfxhbf.mongodb.net/?retryWrites=true&w=majority`;
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
