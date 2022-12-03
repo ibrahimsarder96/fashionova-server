1f0b19943d496ce10ec5eb71d6dec1f0bbd83e06
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2022-12-03 21:52:12 +0600

    database connected

--- index.js
+++ index.js
@@ -12,10 +12,25 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cbfxhbf.mongodb.net/?retryWrites=true&w=majority`;
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run () {
+  try{
+    await client.connect();
+   
+  }
+  catch{
+
+  }
+};
+run().catch(console.dir);
+
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
