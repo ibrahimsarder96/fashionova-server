2870ef21bc064927cd0456ea36301e605c233e0c
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2022-12-04 22:09:46 +0600

    single product load

--- index.js
+++ index.js
@@ -3,15 +3,13 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+// middle ware
 app.use(cors());
 app.use(express.json());
 
 // connection mongodb
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cbfxhbf.mongodb.net/?retryWrites=true&w=majority`;
-
-
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -19,13 +17,21 @@ async function run () {
   try{
     await client.connect();
    const productCollection = client.db('fashionova').collection('products');
-    // products load
+   
+    // products load---------------
     app.get('/product', async(req, res) => {
       const query = {}
       const cursor = productCollection.find(query);
       const products = await cursor.toArray();
       res.send(products);
     })
+    // single product load------------
+    app.get('/product/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query={_id: ObjectId(id)};
+      const product = await productCollection.findOne(query);
+      res.send(product);
+    });
   }
   catch{
 
