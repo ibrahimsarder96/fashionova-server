7be219711db2edcb27ef555cac639996b3fded35
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2022-12-16 02:03:43 +0600

    product Manage

--- index.js
+++ index.js
@@ -57,6 +57,13 @@ async function run () {
       const result = await productCollection.insertOne(newProduct);
       res.send(result);
     });
+    // Product Remove database ------------
+    app.delete('/product/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
+    });
     // // stock available------------------
     // app.get('/available', async(req, res) => {
     //   const product = req.query.product;
